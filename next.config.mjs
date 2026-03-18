f14f1593e7172c9085f81decb234274980b75546
352pcs <tech352pcs@outlook.com>
2026-03-18 02:39:58 +0100

    Configure for Plan B

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This is the magic line
-  images: { unoptimized: true } 
+  output: 'export',      // This is the magic line for Plan B
+  images: {
+    unoptimized: true,   // Required because GitHub doesn't have an image server
+  },
 };
+
 export default nextConfig;
